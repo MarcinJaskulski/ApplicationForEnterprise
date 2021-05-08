129b5117fc4fdae842451ebfa2e918fbc19f66a1
Marcin Jaskulski <jaskulski.marcin@outlook.com>
2021-05-08 14:17:22 +0200

    asp-mvc

---
+++ Lab8 - ASPMVC/MvcNews/MvcNews/Models/NewsItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcNews.Models
+{
+    public class NewsItem
+    {
+        public int Id { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime TimeStamp { get; set; }
+        [Required]
+        [StringLength(140, MinimumLength = 5)]
+        public string Text { get; set; }
+        // Warte uwagi!
+        // Tworzy znacznik czasowy. Jeśli próbuję zmodyfikować rekord, który zmodyfikowałem wcześniej,
+        // a o tymn nie wiem, to znaczniki się nie zgadzają i rzucany jest Exception.
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+    }
+}
